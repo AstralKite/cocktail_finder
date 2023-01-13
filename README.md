a647349bf14244925ae582068d36aaedac0495ab
Fausto Ramirez <ramirezmedia@gmail.com>
2023-01-12 23:11:21 -0500

    Removed a lot. Deleted the 2 coloumns onthe favorites table(deleted the code for the delete button and ratings buttons). Removed the Radio button search. Simplified the Cocktail finder to send it for review. Updated Some of the ReadMe file.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# cocktail_finder
+# Cocktail Finder webpage
+This course project made for FlatIron school that allows you to search through a database of cocktails (provided by [The Cocktail DB](https://www.thecocktaildb.com) ).
+
+* You can learn how to make a cocktail
+* You can save your favorites
+
+---
+
+# Walkthrough
+At the start of the page, you will find the search bar with a Submit button that will show a list of cocktails.
+Click each cocktail to pull up the cocktails information. Hit the Like button to add a cocktail to the favorites list.
+The favorites list will have 2 cocktails already added.
+
+---
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+// Things to do to prepare for phase1 review
+    // understand your code to explain it well - defintly exlpainthe array.find() and how yo uuse it
+    // Justify using the DOMContentLoaded event
+    // create the READEME file
+    // create the video walkthroughof the how to use the webpage
 
-
-
-window.addEventListener('DOMContentLoaded', (event) => {
+//difference between window and document.addEventLuistener
+document.addEventListener('DOMContentLoaded', (event) => {
     
     buildFavCocktailsList();
 
@@ -19,7 +23,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + input;
 
         let count = ul.children.length;
-        //remove current list from UL
+        //remove current list from UL - to refresh search results
         for(let i = count-1; i >= 0; i--){
             ul.children[i].remove();
         }
@@ -63,7 +67,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     let table = document.querySelector("#fav_table");
                     let newRow = document.createElement("tr");
                     let col_1 = document.createElement("td");
-                    let col_3 = document.createElement("td");
 
                     //setup coloumn 01
                     let favCocktailName_p = document.createElement('p');
@@ -73,21 +76,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     favCocktail_img.id = "fav_img_ph";
                     col_1.append(favCocktailName_p, favCocktail_img);
 
-                    //setup column 02
-                    // ratingColoumn() -> called in the newRow.append()
-
-                    //setup coulmn 03
-                    let delete_btn = document.createElement('button');
-                    delete_btn.textContent = "X";
-                    col_3.append(delete_btn);
-
                     //------------------------------------------add new cocktail to DB
                     addFavCocktailToDB(cocktailName.textContent, img.src)
 
-                    newRow.append(col_1, ratingColoumn(), col_3);
+                    newRow.append(col_1);
                     table.append(newRow);
-                    //cocktail name => cocktailName.textContent
-                    //cocktail image => img.src
                 })
 
                 //add click event to each li - click adds cocktail to a preview section
@@ -99,12 +92,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     if(div_delete){
                         div_delete.remove();
                     }
-      
-                    //append items to div
+                    //append items to div that displays the cocktail info
                     div.append(img, cocktailName, glassTypeUsed, instrusctionsText, fav_button);
                     div_main.append(div);
                 });//---end liclick event
-                //append li to UL
                 ul.append(li);
             }//----end for loop to create li cocktails
         })//----- end fetch request
@@ -133,80 +124,12 @@ function ratingColoumn(){
         option3.textContent = "excellent";
 
         option3.selected = "selected";
-    
-    //                          <td>
-    //                             <select name="rating" id="cocktail_rating_id">
-    //                             <option value="average">average</option>
-    //                             <option value="tasty">tasty</option>
-    //                             <option value="excellent">excellent</option>
-    //                             </select>
-    //                         </td>
 
     select.append(option0, option1, option2, option3);
     td.append(select);
     return td;
 }
 
-
-function ratingColoumnFromDB(data){
-    let td = document.createElement('td');
-    let select = document.createElement('select');
-
-
-    //select may need an onChange() function to handle option changes
-
-    select.name = data.id;
-    select.id = "cocktail_rating_id";
-        let option0 = document.createElement('option');
-        option0.value = 0;
-        option0.textContent = "-- unrated --";
-        let option1 = document.createElement('option');
-        option1.value = 1;
-        option1.textContent = "average";
-        let option2 = document.createElement('option');
-        option2.value = 2;
-        option2.textContent = "tasty";
-        let option3 = document.createElement('option');
-        option3.value = 3;
-        option3.textContent = "excellent";
-
-        let optionArray = [option0, option1, option2, option3];
-        //console.log(optionArray.find( (element) => element.value == data.rating));
-        optionArray.find( (element) => element.value == data.rating).selected = "selected";
-        
-
-        //--------------------------------------------------  on change event for current dropdown select
-        select.addEventListener("change", (e)=>{
-            // console.log(e.target.value)
-            // console.log(e.target)
-
-            //GET request of the current DB - Add to JS object - Find match of Select.name && Data.id -- on match go inside id's parent object and update the rating
-            
-            let selectPosition = parseInt(select.name);
-
-            //create placeholder object of DB
-            let tempDB = [];
-            
-            fetch("http://localhost:3000/cocktailsDAT")
-            .then( (resp) => resp.json())
-            .then( (data) => {
-                console.log(data);
-                for(cocktail in data){    
-                   if(selectPosition == data[cocktail].id){
-                    console.log(data[cocktail].name);
-                    data[cocktail].rating = selectPosition;
-                    console.log(data);
-                   }
-                }
-            })
-
-            //---------------------------------  make POST to update DB rating
-        });
-    
-    select.append(option0, option1, option2, option3);
-    td.append(select);
-    return td;
-}
 
 //------------------------------------------------------------POST new fav selection to Database
 function addFavCocktailToDB(name_dat, imgURL_dat){
@@ -218,7 +141,6 @@ function addFavCocktailToDB(name_dat, imgURL_dat){
         imageUrl: imgURL_dat,
         rating: 0
     }
-
 
     //POST that created object
     fetch("http://localhost:3000/cocktailsDAT", {
@@ -239,14 +161,10 @@ function buildFavCocktailsList(){
     .then( (resp) => resp.json())
     .then( (data) => {
         for(cocktail in data){           
-            //name = data[cocktail].name;
-            //imageUrl = data[cocktail].imageUrl;
-            //rating = data[cocktail].rating;
 
             let table = document.querySelector("#fav_table");
                     let newRow = document.createElement("tr");
                     let col_1 = document.createElement("td");
-                    let col_3 = document.createElement("td");
 
                     //setup coloumn 01
                     let favCocktailName_p = document.createElement('p');
@@ -256,18 +174,8 @@ function buildFavCocktailsList(){
                     favCocktail_img.id = "fav_img_ph";
                     col_1.append(favCocktailName_p, favCocktail_img);
 
-                    //setup column 02
-                    // ratingColoumn() -> called in the newRow.append()
-
-                    //setup coulmn 03
-                    let delete_btn = document.createElement('button');
-                    delete_btn.textContent = "X";
-                    col_3.append(delete_btn);
-
-                    newRow.append(col_1, ratingColoumnFromDB(data[cocktail]), col_3);
+                    newRow.append(col_1);
                     table.append(newRow);
-                    //cocktail name => cocktailName.textContent
-                    //cocktail image => img.src
         }
     })
 }
